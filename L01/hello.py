667c22dab8afbae03a7951448dee904bdf98a31b
Benedikt Grether <benedikt@pixelarbeit.de>
2018-10-07 23:37:20 +0200

    Work on exercise

--- L01/hello.py
+++ L01/hello.py
@@ -7,3 +7,8 @@ if age >= 18:
     print("Your're old!!!")
 else:
     print("You're young!!!")
+
+
+# inputField = raw_input("Gib bitte etwas Text ein: ")
+inputField = raw_input("Input: ")
+print(inputField)
