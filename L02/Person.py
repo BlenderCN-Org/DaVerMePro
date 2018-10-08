306664a75b3ea3fd6cc3f84f6243ba7bfb4eeb01
Benedikt Grether <benedikt@pixelarbeit.de>
2018-10-08 15:31:50 +0200

    Edit class

--- L02/Person.py
+++ L02/Person.py
@@ -1,10 +1,13 @@
 class Person:
-    pass
+    # pass
+    def __init__(self, name, nummer):
+        self.Name = name
+        self.Nummer = nummer
 
-nePerson = Person()
+nePerson = Person("Horst", 42)
 
+# nePerson = Person()
+# nePerson.Name = "Horst"
+# nePerson.Nummer = 42
 
-nePerson.Name = "Horst"
-nePerson.Nummer = 42
-
-print("Hier ist " +nePerson.Name + " mit der Nummer" + str(nePerson.Nummer))
+print("Hier ist " +nePerson.Name + " mit der Nummer " + str(nePerson.Nummer))
