36d7f471d222aa93777c073dbe7302a5972af2f2
Benedikt Grether <benedikt@pixelarbeit.de>
2018-10-08 15:24:35 +0200

    Edit prog

--- L02/prog.py
+++ L02/prog.py
@@ -1,5 +1,24 @@
-a = "Test"
-b = 3.1415
-c = 42
-d = True
-e = 1 + 2j
+# a = "Test"
+# b = 3.1415
+# c = 42
+# d = True
+# e = 1 + 2j
+
+# neListe = [3, 2, 5, 4, 1]
+# neAndereListe = ["Anna", "Lea", "Sarah"]
+
+# neListe.append([3, 4, 5])
+# dritteListe = [42, "Hello", 2 +5j]
+# print(dritteListe[0])
+
+# phonebook = {"Müller":2529, "Pietsch":2528, "Fries":2526}
+# print(phonebook["Müller"])
+
+userInput = int(input("Zahl> "))
+
+if userInput == 0:
+    print("Null")
+elif userInput == 1:
+    print("One")
+else:
+    print("All")
