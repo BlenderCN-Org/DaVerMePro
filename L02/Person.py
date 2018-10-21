e776c4599c33a5cd1adcaf034e97dd3563d227c3
Benedikt Grether <benedikt@pixelarbeit.de>
2018-10-21 23:23:49 +0200

    Work on Person

--- L02/Person.py
+++ L02/Person.py
@@ -1,13 +1,58 @@
-class Person:
+class Person(object):
     # pass
-    def __init__(self, name, nummer):
-        self.Name = name
-        self.Nummer = nummer
+    def __init__(self, firstName, lastName, Number):
+        self.FirstName = firstName
+        self.LastName = lastName
+        self.Number = Number
 
-nePerson = Person("Horst", 42)
 
-# nePerson = Person()
-# nePerson.Name = "Horst"
-# nePerson.Nummer = 42
+programmRunning = True
+# nePerson = Person("Horst", 42)
 
-print("Hier ist " +nePerson.Name + " mit der Nummer " + str(nePerson.Nummer))
+# # nePerson = Person()
+# # nePerson.Name = "Horst"
+# # nePerson.Nummer = 42
+
+# print("Hier ist " +nePerson.Name + " mit der Nummer " + str(nePerson.Nummer))
+
+personList = []
+personList.append(Person("Max", "Herre", "1"))
+personList.append(Person("Benedikt", "Grether", "2"))
+personList.append(Person("Tobi", "Test", "3"))
+personList.append(Person("Mark", "Foster", "4"))
+personList.append(Person("Smudo", "Irgendwas", "5"))
+
+
+while programmRunning == True:
+    print("Drücke A um alle Personen anzuzeigen")
+    print("Drücke B um eine eine neue Person anzulegen")
+    print("Drücke C um eine Person zu ändern")
+    print("Drücke D um eine Person zu löschen")
+    print("Drücke E um das Programm zu beenden")
+    inputCases = input("Ihre Eingabe > ")
+        
+    if inputCases == "A":
+        for person in personList:
+            print(person.FirstName, person.LastName, person.Number)
+    
+    elif inputCases == "B":
+        firstNameInput = input("Bitte Vorname eingeben > ")
+        lastNameInput = input("Bitte Nachname eingeben > ")
+        numberInput = input("Bitte Personalnummer eingeben > ")
+        personList.append(Person(firstNameInput, lastNameInput, numberInput))
+
+    elif inputCases == "C":
+        changePersonInput = input("Welche Person möchten sie verändern? > ")
+        personList.index(Person (changePersonInput))
+        # for person in personList:
+        #     if changePersonInput in person.FirstName:
+        #         firstNameInput = input("Bitte Vorname eingeben >")
+        #         lastNameInput = input("Bitte Nachname eingeben >")
+        #         numberInput = input("Bitte Personalnummer eingeben >")
+                #personList.index(changePersonInput)
+    elif inputCases == "D":
+        deletePersonInput = input("Welche Person soll entfernt werden > ")
+
+    elif inputCases == "E":
+        print("Das Programm wird nun Beendet")
+        programmRunning = False
---
+++ L02/datatyp.py
@@ -0,0 +1,37 @@
+x = 5
+y = 2
+#z = x + y
+print("x + y " + str(x + y))
+# print("x + y ")
+print("x -y = " + str(x - y))
+print ("x % y = " + str(x % y))
+print("-x = "  + str(-x))
+print("x * y = " + str(x * y))
+print("x ** y = " + str(x ** y))
+print("x // y = " + str(x // y))
+
+x = 2
+y = 3
+z = x + y
+x = z
+y = x
+print(x)
+print(y)
+print(z)
+
+i = 0.1 + 0.1 + 0.1 - 0.3
+print(i)
+i = 5 + 10**40 - 10**40
+print(i)
+i = 5.0 + 10**40 - 10**40
+print(i)
+
+markA = 2.74
+markB = 2.12
+markC = 2.43
+average = 2.00
+
+mark = (markA + markB + markC) / average
+print(mark)
+
+
